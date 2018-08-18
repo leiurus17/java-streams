b8c09f0a5ec02d882c988c132886e3c262717cb4
Marlon Monzon <martianworm17@gmail.com>
2018-08-18 20:00:37 +0800

    Stream rows from text file and save to List

---
+++ src/main/java/com/gowpar/streams/JavaStreamsNine.java
@@ -0,0 +1,25 @@
+package main.java.com.gowpar.streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JavaStreamsNine {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = System.getProperty("user.dir");
+        String anotherPath = "/target/production/java-streams/main/java/com/gowpar/streams/";
+
+        // 8. Stream rows from text file and save to List
+        // WTF with the hacked path...
+        List<String> bands = Files.lines(Paths.get(path + anotherPath + "bands.txt"))
+                .filter(x -> x.contains("lo"))
+                .collect(Collectors.toList());
+
+        bands.forEach(x -> System.out.println(x));
+    }
+}
